70e114a4624f3a417b1d9b77ab50abec86e22aaa
Breck Yunits <breck7@gmail.com>
2017-10-30 18:22:11 -0700

    Updated to latest Tree

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-module.exports = require("./src/packProgram.js")
+const TreeProgram = require("treeprogram")
+const fs = require("fs")
+const packGrammar = fs.readFileSync(__dirname + "/pack.grammar", "utf8")
+
+class packProgram extends TreeProgram {
+  getGrammarString() {
+    return packGrammar
+  }
+
+  getGrammarFilePath() {
+    return __dirname + "/pack.grammar"
+  }
+
+  compile() {
+    return this.toJson()
+  }
+}
+
+module.exports = packProgram
